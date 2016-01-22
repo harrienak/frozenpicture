088c46c31834614516558afb95ed1f835b997f64
harrienak <sjoerdmoret6@gmail.com>
2016-01-22 10:48:36 +0100

    startscreen

---
+++ app/src/main/java/com/geckoapps/raaddeplaat/activity/StartActivity.java
@@ -0,0 +1,16 @@
+package com.geckoapps.raaddeplaat.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.geckoapps.raaddeplaat.R;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+}
